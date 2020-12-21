1952b181305393aef54238c36327329a0f93406d
HugoPoletto34 <67022441+HugoPoletto34@users.noreply.github.com>
2020-12-20 21:36:48 -0300

    commit on mastet

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    res.statusCode(200).send({message: "CERTASSO meu caro amigo tixinha!"})
+    res.status(200).send({message: "CERTASSO meu caro amigo tixinha!"})
 });
 
 module.exports = app;
